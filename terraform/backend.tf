355cac7199ee12ea0a1f078d7fb97e2f03d30379
OmTalent 54 <omtalent54@OmTalents-MacBook-Air.local>
2026-07-27 15:46:59 +0200

    m

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "project-2-503411-tf-state-20260727" # Must match the bucket created by Terraform or pre-exist
+    bucket = "project-2-503411-tf-state-20260727-01" # Must match the bucket created by Terraform or pre-exist
     prefix = "terraform/state"
   }
 } 
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "terraform_state" {
-  name     = "${var.project_id}-tf-state-20260727"
+  name     = "${var.project_id}-tf-state-20260727-01"
   location = var.region
 
   versioning {
