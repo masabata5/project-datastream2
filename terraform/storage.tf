b738e491178572740c34b170400496a01fe3a23c
OmTalent 54 <omtalent54@OmTalents-MacBook-Air.local>
2026-07-28 11:44:10 +0200

    storage fix

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,12 +1,1 @@
-resource "google_storage_bucket" "terraform_state" {
-  name     = "${var.project_id}-tf-state-20260727-01"
-  location = var.region
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-} 
+ 
