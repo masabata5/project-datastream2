9985505969dc27453614336656584d57f8b3e183
PShungube <pshungube@jutomate.com>
2026-07-28 12:02:00 +0200

    Changed the project ID

--- terraform/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "terraform_state" {
-  name     = "${var.project_id}-terraform-state"
-  location = var.region
-
-  versioning {
-    enabled = true
-  }
-} 
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,1 +1,1 @@
-project_id = "project-repo-498812"
+project_id = "project-2-503411"
